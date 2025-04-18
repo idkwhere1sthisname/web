661baeecb7eb0af248f7b0e4bbe3528179c4636e
idkwhereisthisname <comestaix@gmail.com>
2025-04-18 20:06:34 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,236 @@
+function changelink(id) {
+    var element = document.getElementById(id);
+
+    element.classList.remove('link');
+    element.classList.add('visited');
+}
+
+function copy() {
+    const text = "idkwhereisthisname";
+    let alertMsg = "username successfully copied to your clipboard!";
+    let errMsg = "failed to copy to clipboard, please try again";
+
+    navigator.clipboard.writeText(text).then(() => {
+        alert(alertMsg);
+    }).catch (err => {
+        alert(errMsg);
+    });
+};
+
+class Main {
+    constructor() { };
+}
+
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+class webSound {
+    constructor() { };
+    playSound(id) {
+        // for MP3 files
+        var SE_Hover = 1;
+        var SE_Coin = 2;
+        var SE_Load = 3;
+        var SE_Finish = 4;
+        var SE_Back = 10;
+        var SE_Init = 11;
+        var SE_Back2 = 12;
+        var SE_OldOK = 13;
+        var SE_Error = 14;    
+        let SE = new Audio('sound/' + id + '.mp3');
+        SE.play();
+    }
+    playBGM(path) {
+        let bgmPath = path
+        let bgm = new Audio(bgmPath);
+
+        bgm.play();
+        bgm.loop = true;
+        bgm.currentTime = 0;
+    }
+    stopBGM() {
+        let bgmPath = '/sound/bgm.mp3';
+        let bgm = new Audio(bgmPath);
+
+        bgm.pause();
+    }
+    playStartupSound() {
+        let sndPath = '/sound/startup.wav';
+        let snd = new Audio(sndPath);
+
+        snd.play();
+        snd.loop = false;
+    };
+    playSE(id) {
+        // for WAV files
+        var SE_Hover = 1;
+        var SE_Coin = 2;
+        var SE_Load = 3;
+        var SE_Finish = 4;
+        var SE_Back = 10;
+        var SE_Init = 11;
+        var SE_Back2 = 12;
+        var SE_OldOK = 13;
+        var SE_Error = 14;
+    
+        let SE = new Audio('/sound/' + id + '.wav');
+        SE.play();
+    }
+    Load() {
+        var loadingsfx = new Audio("BGM/load.wav");
+        document.getElementById("loadicon").style.display="block";
+        //setTimeout(this.stopLoad,2000);
+        loadingsfx.play();
+        loadingsfx.loop=true;
+        loadingsfx.currentTime = 0;
+    }
+    stopLoad() {
+        var loadingsfx = new Audio("BGM/load.wav");
+        loadingsfx.play();
+        loadingsfx.loop=false;
+        document.getElementById("loadicon").style.display="none";
+        loadingsfx.pause();
+    }
+    playDSCameraTutorialBGM() {
+        const audio = document.getElementById('DSiCamTutElem');
+        if (audio) {
+            audio.play();
+            audio.loop = true;
+        } else { console.error('no element') }
+    }
+    playDSIMenuBGM() {
+        const aud = document.getElementById("DSMenuElem");
+        aud.play();
+        aud.loop = true;
+    }
+    play3DSInvalidBannerSND() {
+        let aud = new Audio("/BGM/3dsinvalidBanner.mp3");
+        aud.play();
+        aud.loop = true;
+    }
+    play3DSLockedBannerSND() {
+        let aud = new Audio("/BGM/3dslockedBanner.mp3");
+        aud.play();
+        aud.loop=true;
+    }
+}
+
+function jumpTo(item, type = "url") {
+    if (type == "url") {
+        snd.playSE(15);
+        snd.Load();
+        setTimeout(() => {
+            window.location.href = item;
+        }, 550)
+    } else if (type == "reload" && item == null) {
+       snd.playSE(15);
+       snd.Load();
+       setTimeout(() => {
+            window.location.reload();            
+       }, 550)
+    }
+}
+
+function getFormattedDate() {
+    const date = new Date();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return month + day;
+}
+
+function getMusic() {
+    const date = getFormattedDate();
+
+    if (date === "0401") {
+        return "/bgm/Secret.mp3";
+    }
+
+    switch (date) {
+        case "0327":
+            return "/bgm/eshop2011.wav";
+        case "0130":
+            return "/bgm/wsc.mp3";
+        case "0727":
+            return "/bgm/ww.mp3";
+        case "0331":
+            return "/bgm/dsshop.mp3";
+        default:
+            return "/bgm/bgm.mp3";
+    }
+}
+
+function dispMsg() {
+    const date = getFormattedDate();
+    const yr = new Date().getFullYear().toString().slice(-2);
+    const constelems = "<br/><br/>";
+    let msg = "";
+
+    switch (date) {
+        case "0327":
+            msg = constelems + (yr - 23) + " years have passed since the 3DS/Wii U eShop shutdown.";
+            break;
+        case "0401":
+            msg = constelems + "Happy April Fools!...";
+            break;
+        case "0130":
+            msg = constelems + (yr - 19) + " years have passed since the Wii Shop Channel shutdown.";
+            break;
+        case "0331":
+            msg = constelems + (yr - 17) + " years have passed since the DSi Shop shutdown.";
+            break;
+    }
+
+    document.getElementById("Obj").innerHTML = msg;
+}
+
+let Music = getMusic();
+
+const cro = new URLSearchParams(window.location.search).has('cro');
+
+let bgmPath = getMusic();
+let bgm = new Audio(bgmPath);
+
+function playBGM() {
+    let elem = document.getElementById("shopbgmselector");
+    document.getElementById("shopbgm").innerHTML = "pause BGM";
+    let savetime = localStorage.getItem("bgmlooppoint");
+
+    elem.href = "javascript:pauseBGM()";
+    bgm.volume = 0.6;
+    bgm.loop = true;
+
+    bgm.play().then(() => {
+        if (savetime) {
+            bgm.currentTime = parseFloat(savetime);
+        }
+    });
+}
+
+function pauseBGM() {
+    localStorage.setItem("bgmlooppoint", bgm.currentTime);
+    bgm.pause();
+    document.getElementById("shopbgmselector").href = "javascript:playBGM();";
+    document.getElementById("shopbgm").innerHTML = "play BGM";
+}
+
+window.onbeforeunload = function() {
+    localStorage.setItem("bgmlooppoint", bgm.currentTime);
+};
